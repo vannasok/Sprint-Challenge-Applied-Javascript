ae09cf502379d11f9104d224758d76cff920cdca
vannasok92 <vannasok92@yahoo.com>
2019-09-06 09:00:26 -0700

    header finish

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,32 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const hdShowcase = document.querySelector('.header-container');
+
+hdShowcase.appendChild(Header());
+
+function Header() {
+
+ const 
+ headerShowcase = document.createElement('div'),
+ headerDate = document.createElement('span'),
+ headerTitle = document.createElement('h1'),
+ headerTemp = document.createElement('span');
+
+ headerShowcase.appendChild(headerDate);
+ headerShowcase.appendChild(headerTitle);
+ headerShowcase.appendChild(headerTemp);
+
+ headerShowcase.classList.add('header');
+ headerDate.classList.add('date');
+ headerTemp.classList.add('temp');
+
+ headerTitle.textContent = 'Lambda Times';
+ headerDate.textContent = 'September 6th, 2019';
+ headerTemp.textContent = 80,'&deg;c';
+
+ return headerShowcase;
+
+}//header function
+
+
